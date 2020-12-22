ce0114e1ed3e55a007286d8109ad03b4422c2ad2
steven-rr <steverelativity@gmail.com>
2020-12-22 02:37:34 -0500

    adding progress. leaving off on video 17.

---
+++ notes.cpp
@@ -0,0 +1,15 @@
+/*
+
+so I want to learn the following:
+
+nav bar.
+able to include images, and background.
+dynamic font. 
+
+Aligning text to box, and put wherever i want in fancy font that i want.
+flexible website to be able to fit in any website.
+
+
+
+
+*/
