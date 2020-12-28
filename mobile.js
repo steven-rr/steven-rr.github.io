ab0118ec49b9a1ad2505208ab5e206c2284f205f
steven-rr <steverelativity@gmail.com>
2020-12-28 01:43:01 -0500

    deply showing fa-fa-bars

--- mobile.js
+++ mobile.js
@@ -1,24 +1,27 @@
-// remove nav main and social media from visibility.
-let nav_main = document.querySelector(".nav-main");
-let nav_social = document.querySelector(".social-media");
-nav_main.style.opacity = 0;
-nav_social.style.opacity = 0;
+// let nav_mobile = document.getElementById("mobile-toggle");
+// nav_mobile.style.opacity=  1;
+
+// // remove nav main and social media from visibility.
+// let nav_main = document.querySelector(".nav-main");
+// let nav_social = document.querySelector(".social-media");
+// nav_main.style.opacity = 0;
+// nav_social.style.opacity = 0;
 
 
 
-// add nav-mobile child.
-const nav_mobile = document.createElement("nav_mobile");
-nav_mobile.style.opacity = 1;
-nav_mobile.innerHTML = `<ul>
-<li><a href = "https://www.facebook.com/stevenator9001/" > <i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-<li><a href = "https://github.com/steven-rr" ><i class="fa fa-github" aria-hidden="true"></i></a></li>
-<li><a href = "https://www.linkedin.com/in/steven-rivadeneira" ><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-<li> <a href = "https://www.instagram.com/steven__rr/" ><i class="fa fa-instagram" aria-hidden="true"></i></a> </li>
-</ul>`;
+// // add nav-mobile child.
+// const nav_mobile = document.createElement("nav_mobile");
+// nav_mobile.style.opacity = 1;
+// nav_mobile.innerHTML = `<ul>
+// <li><a href = "https://www.facebook.com/stevenator9001/" > <i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+// <li><a href = "https://github.com/steven-rr" ><i class="fa fa-github" aria-hidden="true"></i></a></li>
+// <li><a href = "https://www.linkedin.com/in/steven-rivadeneira" ><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+// <li> <a href = "https://www.instagram.com/steven__rr/" ><i class="fa fa-instagram" aria-hidden="true"></i></a> </li>
+// </ul>`;
 
-let nav = document.querySelector("nav");
-val = nav;
-nav.appendChild(nav_mobile);
+// let nav = document.querySelector("nav");
+// val = nav;
+// nav.appendChild(nav_mobile);
 
 
 // // add fa-fa bars and styling to nav-mobile child.
@@ -29,5 +32,13 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
     // add 0 opacity to main icons.
     //move social media icons
     // add fa-fabars.   
+    let nav_mobile = document.getElementById("mobile-toggle");
+    nav_mobile.style.opacity=  1;
+
+    // remove nav main and social media from visibility.
+    let nav_main = document.querySelector(".nav-main");
+    let nav_social = document.querySelector(".social-media");
+    nav_main.style.opacity = 0;
+    nav_social.style.opacity = 0;
     
 }
