87e9c7091ec16db743730e1a5fd21fcd36b97d95
steven-rr <steverelativity@gmail.com>
2021-09-04 14:01:21 -0400

    incorporate rudimentary active classes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,15 +8,53 @@ const Navbar = () => {
     return (
         <div className={NavbarCSS.containerMain}>
                 <div className={NavbarCSS.navHome}>
-                    <Link to="home" spy={true} smooth='easeInQuart'>HOME</Link> 
+                    <Link 
+                    to="home" 
+                    spy={true} 
+                    smooth='easeInQuart'
+                    activeClass={NavbarCSS.active}>
+                        HOME
+                    </Link> 
                 </div>
 
                 <div className={NavbarCSS.navMain}>
                     <ul className={NavbarCSS.navMainUl}>
-                        <li className={NavbarCSS.navMainLi}><Link to="about"  spy={true} smooth='easeInQuart'>ABOUT</Link></li>
-                        <li className={NavbarCSS.navMainLi}><Link to="timeline"  spy={true}  smooth='easeInQuart'>TIMELINE</Link></li>
-                        <li className={NavbarCSS.navMainLi}><Link to="projects"  spy={true}  smooth='easeInQuart' >PROJECTS</Link></li>
-                        <li className={NavbarCSS.navMainLi}><Link to="contact"  spy={true}  smooth='easeInQuart' >CONTACT</Link></li>
+                        <li className={NavbarCSS.navMainLi}>
+                            <Link 
+                            to="about"  
+                            spy={true} 
+                            smooth='easeInQuart'
+                            activeClass={NavbarCSS.active}>
+                                ABOUT
+                            </Link>
+                        </li>
+                        <li className={NavbarCSS.navMainLi}>
+                            <Link 
+                            to="timeline"  
+                            spy={true}  
+                            smooth='easeInQuart'
+                            activeClass={NavbarCSS.active}>
+                                TIMELINE
+                            </Link>
+                        </li>
+                        <li className={NavbarCSS.navMainLi}>
+                            <Link 
+                            to="projects"  
+                            spy={true}  
+                            smooth='easeInQuart'
+                            activeClass={NavbarCSS.active}>
+                                PROJECTS
+                            </Link>
+                        </li>
+                        <li className={NavbarCSS.navMainLi}>
+                            <Link 
+                            to="contact"  
+                            spy={true}  
+                            smooth='easeInQuart'
+                            activeClass={NavbarCSS.active}>
+                                CONTACT
+                            </Link>
+                        </li>
                         <li className={NavbarCSS.navMainLi}><a href={resumePDF} download="StevenRivadeneiraResume.pdf">RESUME </a></li>
                     </ul>
                 </div>
