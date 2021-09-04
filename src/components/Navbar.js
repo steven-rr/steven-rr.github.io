867826c86d11cdd6279c705fa36f4a43018de15e
Steven Rivadeneira <steverelativity@gmail.com>
2021-09-04 00:31:06 -0400

    resume download link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import NavbarCSS from './Navbar.module.css'
 import * as Scroll from 'react-scroll';
 import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+import resumePDF from "../Resume.pdf"
 
 const Navbar = () => {
     return (
@@ -15,7 +16,8 @@ const Navbar = () => {
                         <li><Link to="about"  spy={true} smooth='easeInQuart'>ABOUT</Link></li>
                         <li><Link to="timeline"  spy={true}  smooth='easeInQuart'>TIMELINE</Link></li>
                         <li><Link to="projects"  spy={true}  smooth='easeInQuart' >PROJECTS</Link></li>
-                        <li><a href="../Resume.pdf">RESUME </a></li>
+                        <li><Link to="contact"  spy={true}  smooth='easeInQuart' >CONTACT</Link></li>
+                        <li><a href={resumePDF} download="StevenRivadeneiraResume.pdf">RESUME </a></li>
                     </ul>
                 </div>
                 
